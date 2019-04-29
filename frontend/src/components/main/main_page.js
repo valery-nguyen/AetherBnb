15d758110c5f616b495e22ae88a8e2b7f09fe368
valery-nguyen <valery.nguyen@ucla.edu>
2019-04-28 20:46:25 -0400

    set up basic frontend auth

---
+++ frontend/src/components/main/main_page.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class MainPage extends React.Component {
+
+  render() {
+    return (
+      <div>
+        <h1>A new_project Clone</h1>
+        <footer>
+          Copyright &copy; 2019 new_project
+        </footer>
+      </div>
+    );
+  }
+}
+
+export default MainPage;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,9 +1,39 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
+import Root from './components/root';
+import configureStore from './store/store';
+import jwt_decode from 'jwt-decode';
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+import { setAuthToken } from './util/session_api_util';
+import { logout } from './actions/session_actions';
+
+import axios from 'axios';
+
+document.addEventListener('DOMContentLoaded', () => {
+  let store;
+
+  if (localStorage.jwtToken) {
+    setAuthToken(localStorage.jwtToken);
+
+    const decodedUser = jwt_decode(localStorage.jwtToken);
+    const preloadedState = { session: { isAuthenticated: true, user: decodedUser } };
+
+    store = configureStore(preloadedState);
+
+    const currentTime = Date.now() / 1000;
+
+    if (decodedUser.exp < currentTime) {
+      store.dispatch(logout());
+      window.location.href = '/login';
+    }
+  } else {
+    store = configureStore({});
+  }
+  const root = document.getElementById('root');
+
+  ReactDOM.render(<Root store={store} />, root);
+
+
+  // remove the commands below after development
+  window.axios = axios;
+});
