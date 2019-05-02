f8ce8082c173a1b279a07bd567a9887878df3766
Colin <colin.reitman87@gmail.com>
2019-05-02 16:07:15 -0400

    work on marker-labels

--- frontend/src/util/marker_manager.js
+++ frontend/src/util/marker_manager.js
@@ -1,13 +1,17 @@
+import { createPopupClass } from './popup_util';
+import React from 'react';
 const google = window.google;
+
 
 export default class MarkerManager {
   constructor(map) {
     this.map = map;
     this.markers = {};
+
+    this.Popup = createPopupClass();
   }
 
   updateMarkers(spots) {
-    console.log('updating map');
     spots.forEach((spot) => {
       if (!Object.keys(this.markers).includes(spot.id)) {
         this.createMarkerFromSpot(spot);
@@ -19,13 +23,35 @@ export default class MarkerManager {
     // debugger;
     // const pos = new google.maps.LatLng(spot.lat, spot.lng);
     const position = { lat: spot.lat, lng: spot.lng };
+    // var iconBase = 'https://maps.google.com/mapfiles/kml/shapes/';
+
+    // let content = document.createElement('div');
+    // content.innerHTML = (<div>${spot.price}</div>);
+    // let popup = new this.Popup(
+    // position,
+    // content);
+    // popup.setMap(this.map)
+
     const marker = new google.maps.Marker({
       position,
       map: this.map,
-      spotId: spot._id
+      label: `$${spot.price}`,
+      spotId: spot._id,
+      
     });
+
+
     // marker.addListener('click', () => this.handleClick(bench));
+
+
     this.markers[marker.spotId] = marker;
   }
 
+
+
 }
+
+// icon: {
+//   url: 'https://www.pinclipart.com/picdir/big/13-136680_download-square-speech-bubble-clipart-speech-balloon-rectangular.png',
+//     scaledSize: new google.maps.Size(32, 32)
+// }
