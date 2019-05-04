b9fd00330417ea81431ebfb426a6e3f1b95e96a5
valery-nguyen <valery.nguyen@ucla.edu>
2019-05-04 14:39:51 -0400

    set up for heroku

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+const path = require('path');
+
 const express = require("express");
 const app = express();
 const db = require('./config/keys').mongoURI;
@@ -8,6 +10,13 @@ const passport = require('passport');
 const users = require("./routes/api/users");
 const spots = require("./routes/api/spots");
 const bookings = require("./routes/api/bookings");
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('frontend/build'));
+  app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+  })
+}
 
 mongoose
   .connect(db, { useNewUrlParser: true })
