404c1a61ad72cf202065bd0aa453630742aba314
valery-nguyen <valery.nguyen@ucla.edu>
2019-05-07 23:38:02 -0400

    fixed link to non-secure website

--- frontend/src/components/main/main_page.js
+++ frontend/src/components/main/main_page.js
@@ -7,7 +7,7 @@ class MainPage extends React.Component {
     return (
       <div className="splash-page">
         <h1 className="splash-page-title" >Your home away from home...</h1>
-        <img alt="" className="splash-page" src='http://www.vythiriresort.com/images/gallery/high-resolution/tree_house.jpg' />
+        <img alt="" className="splash-page-img"/>
       </div>
     );
   }
